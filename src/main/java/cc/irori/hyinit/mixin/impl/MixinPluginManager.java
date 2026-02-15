587e2f315fd0267c2a5270a8a5676d5b1c178c25
KabanFriends <yuukun2004@protonmail.ch>
2026-02-16 02:53:03 +0900

    fix: hytale's own classpath plugins not being loaded

--- src/main/java/cc/irori/hyinit/mixin/impl/MixinPluginManager.java
+++ src/main/java/cc/irori/hyinit/mixin/impl/MixinPluginManager.java
@@ -1,8 +1,14 @@
 package cc.irori.hyinit.mixin.impl;
 
+import cc.irori.hyinit.util.UrlUtil;
+import com.hypixel.hytale.Main;
 import com.hypixel.hytale.server.core.plugin.PluginManager;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -11,14 +17,31 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class MixinPluginManager {
 
     @Redirect(
-            method = "setup()V",
+            method = "loadPluginsInClasspath",
             at =
                     @At(
                             value = "INVOKE",
-                            target =
-                                    "Lcom/hypixel/hytale/server/core/plugin/PluginManager;loadPluginsInClasspath(Ljava/util/Map;Ljava/util/Map;)V"))
-    private void hyinit$skipLoadPluginsInClasspath(
-            PluginManager instance, Map<String, Path> pluginJars, Map<String, Path> pluginDirs) {
-        // Classpath plugin loading is temporarily disabled
+                            target = "Ljava/lang/ClassLoader;getResources(Ljava/lang/String;)Ljava/util/Enumeration;"))
+    private Enumeration<URL> hyinit$redirectManifestResources(ClassLoader instance, String name) throws IOException {
+        Path hytaleJarPath =
+                UrlUtil.asPath(Main.class.getProtectionDomain().getCodeSource().getLocation());
+
+        List<URL> urls = new ArrayList<>();
+        Enumeration<URL> resources = instance.getResources(name);
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            URLConnection connection = url.openConnection();
+            if (connection instanceof JarURLConnection jarConnection) {
+                Path jarPath = UrlUtil.asPath(jarConnection.getJarFileURL());
+
+                // TODO: Allow other classpath plugins to be loaded - This impl breaks classpath plugin loading
+                if (jarPath.equals(hytaleJarPath)) {
+                    urls.add(url);
+                }
+            } else {
+                urls.add(url);
+            }
+        }
+        return Collections.enumeration(urls);
     }
 }
